0f61922a7a46fa13c47da444e5e82f63c4f3baa2
Pashckevich227 <iport234@gmail.com>
2023-01-23 22:24:16 +0300

    feat: template for registration

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, RegisterView, LoginView
 
 urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name='post_detail'),
-    path("", BlogListView.as_view(), name='home')
+    path("", BlogListView.as_view(), name='home'),
+    path("register", RegisterView.as_view(), name='register'),
+    path("login", LoginView.as_view(), name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
 
@@ -11,3 +11,14 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class RegisterView(ListView):
+    model = Post
+    template_name = 'register.html'
+
+
+class LoginView(ListView):
+    model = Post
+    template_name = 'login.html'
+
