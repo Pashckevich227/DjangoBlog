8c2b61f98f543f2c89620453780b4a02ab82c157
Pashckevich227 <iport234@gmail.com>
2023-01-24 18:37:38 +0300

    feat: update post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, LoginView, BlogCreateView
+from .views import (BlogListView,
+                    BlogDetailView,
+                    LoginView,
+                    BlogCreateView,
+                    BlogUpdateView,)
 
 urlpatterns = [
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path("post/<int:pk>/", BlogDetailView.as_view(), name='post_detail'),
     path("", BlogListView.as_view(), name='home'),
     path("accounts/login/", LoginView.as_view(), name='login'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Post
 
@@ -13,10 +13,16 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
-class BlogCreateView(CreateView): # новое изменение
+class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
 
 
 class LoginView(ListView):
